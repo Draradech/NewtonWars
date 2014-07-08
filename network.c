4804cf16288cd49661d842c8d357f1afd9922d8c
Manuel Kasten <kasten.m@gmx.de>
2014-07-08 13:21:10 +0200

    remove unused function

--- network.c
+++ network.c
@@ -70,17 +70,6 @@ static void print_error(const char* msg)
    #endif
 }
 
-// get sockaddr, IPv4 or IPv6:
-static void *get_in_addr(struct sockaddr *sa)
-{
-   if (sa->sa_family == AF_INET)
-   {
-      return &(((struct sockaddr_in*)sa)->sin_addr);
-   }
-
-   return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
-
 static void snd(int socket, char* msg)
 {
    int flags;
