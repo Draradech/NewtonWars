dfedf2b1e2d6154e2ea768a18cd39c23628e03ff
Manuel Kasten <kasten.m@gmx.de>
2015-06-05 22:48:55 +0200

    modified bot slightly

--- nwbot.py
+++ nwbot.py
@@ -21,28 +21,31 @@ s.send("b\n")
 angle = 0
 while True:
    data = s.recv(8)
-   typ, pid = struct.unpack("II", data)
-   if typ == 1:
-      mypid = pid
-      print "My id: %d" % mypid
-   elif typ == 2:
-      print "Player %d left" % pid
-      s.send("%f\n" % angle)
-   elif typ == 3:
-      data = s.recv(8)
-      fx, fy = struct.unpack("ff", data)
-      print "Player %d at %f, %f" % (pid, fx, fy)
-      s.send("%f\n" % angle)
-   elif typ == 4:
-      data = s.recv(4)
-      n = struct.unpack("I", data)[0]
-      print "Player %d finished shot with %d segments:" % (pid, n)
-      while n > 0:
-         n -= 1
-         data = s.recv(8)
-         print repr(struct.unpack("ff", data))
-      if pid == mypid:
-         angle += 5
+   try:
+      typ, pid = struct.unpack("II", data)
+      if typ == 1:
+         mypid = pid
+         print "My id: %d" % mypid
+      elif typ == 2:
+         print "Player %d left" % pid
          s.send("%f\n" % angle)
+      elif typ == 3:
+         data = s.recv(8)
+         fx, fy = struct.unpack("ff", data)
+         print "Player %d at %f, %f" % (pid, fx, fy)
+         s.send("%f\n" % angle)
+      elif typ == 4:
+         data = s.recv(4)
+         n = struct.unpack("I", data)[0]
+         print "Player %d finished shot with %d segments:" % (pid, n)
+         while n > 0:
+            n -= 1
+            data = s.recv(8)
+            #print repr(struct.unpack("ff", data))
+         if pid == mypid:
+            angle += 5
+            s.send("%f\n" % angle)
+   except struct.error:
+      print repr(data)
       
 
