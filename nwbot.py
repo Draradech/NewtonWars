fe1caafd320f479b326065f74fca644d884805b5
Moritz Rosenthal <moritz@mekelburger.org>
2015-06-19 20:37:15 +0200

    Made the nwbot example more pythonic and ported it to python3

--- nwbot.py
+++ nwbot.py
@@ -1,7 +1,12 @@
+import sys
 import socket
 import struct
 import math
 import random
+
+if sys.version_info[0] < 3:
+    def bytes(data, encoding):
+        return data
 
 def readall(s):
    timeout = s.gettimeout()
@@ -22,44 +27,45 @@ def recvall(sock, count):
       count -= len(newbuf)
    return buf
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.2.107", 3490))
+if __name__ == "__main__":
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("127.0.0.1", 3490))
 
-s.send("n Stupobot\n")
-readall(s)
-s.send("b\n")
+    s.send(bytes("n Stupobot\n", 'UTF-8'))
+    readall(s)
+    s.send(bytes("b\n", 'UTF-8'))
 
-angle = random.uniform(-180, 180)
-speed = 10
-mypid = None
-s.send("v %f\n%f\n" % (speed, angle))
+    angle = random.uniform(-180, 180)
+    speed = 10
+    mypid = None
+    s.send(bytes("v %f\n%f\n" % (speed, angle), 'UTF-8'))
 
-while True:
-   data = recvall(s, 8)
-   typ, pid = struct.unpack("II", data)
-   if typ == 1:
-      mypid = pid
-      print "My id: %d" % mypid
-   elif typ == 2:
-      print "Player %d left" % pid
-      s.send("r\n")
-   elif typ == 3:
-      data = recvall(s, 8)
-      fx, fy = struct.unpack("ff", data)
-      print "Player %d at %f, %f" % (pid, fx, fy)
-      s.send("r\n")
-   elif typ == 4:
-      data = recvall(s, 4)
-      n = struct.unpack("I", data)[0]
-      print "Player %d finished shot with %d segments" % (pid, n)
-      while n > 0:
-         n -= 1
-         recvall(s, 8)
-      if pid == mypid:
-         angle += 361 / 3.0
-         s.send("v %f\n%f\n" % (speed, angle))
-   else:
-      print "Unexpected data: %s" % repr(data)
+    while True:
+       data = recvall(s, 8)
+       typ, pid = struct.unpack("II", data)
+       if typ == 1:
+          mypid = pid
+          print ("My id: %d" % (mypid, ))
+       elif typ == 2:
+          print ("Player %d left" % (pid, ))
+          s.send(bytes("r\n", 'UTF-8'))
+       elif typ == 3:
+          data = recvall(s, 8)
+          fx, fy = struct.unpack("ff", data)
+          print ("Player %d at %f, %f" % (pid, fx, fy))
+          s.send(bytes("r\n", 'UTF-8'))
+       elif typ == 4:
+          data = recvall(s, 4)
+          n = struct.unpack("I", data)[0]
+          print ("Player %d finished shot with %d segments" % (pid, n))
+          for i in range(n):
+             data = recvall(s, 8)
+             print ("Courve: %r" % (struct.unpack("ff", data), ))
+          if pid == mypid:
+             angle += 361 / 3.0
+             s.send(bytes("v %f\n%f\n" % (speed, angle), 'UTF-8'))
+       else:
+          print ("Unexpected data: %r" % (data, ))
 
 
 
