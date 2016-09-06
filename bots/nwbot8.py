04a41167c9330d5e8061eaf96429bbaf78a09a3f
Manuel Kasten <kasten.m@gmx.de>
2016-09-06 16:38:40 +0200

    try to switch nwbot8 to OO

--- bots/nwbot8.py
+++ bots/nwbot8.py
@@ -7,90 +7,113 @@ import math
 import random
 import time
 
-if sys.version_info[0] < 3:
-    def bytes(data, encoding):
-        return data
+class Network:
+   def readall(self):
+      timeout = self.s.gettimeout()
+      self.s.settimeout(1)
+      while True:
+         try:
+            self.s.recv(4096)
+         except socket.timeout:
+            break
+      self.s.settimeout(timeout)
 
-def readall(s):
-   timeout = s.gettimeout()
-   s.settimeout(1)
-   while True:
-      try:
-         s.recv(4096)
-      except socket.timeout:
-         break
-   s.settimeout(timeout)
+   def recvall(self, count):
+      buf = b''
+      while count:
+         newbuf = self.s.recv(count)
+         if not newbuf: return None
+         buf += newbuf
+         count -= len(newbuf)
+      return buf
 
-def recvall(sock, count):
-   buf = b''
-   while count:
-      newbuf = sock.recv(count)
-      if not newbuf: return None
-      buf += newbuf
-      count -= len(newbuf)
-   return buf
+   def __init__(self, cnt):
+      self.cnt = cnt
+      self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      self.s.connect(("127.0.0.1", 3490))
 
+      self.s.send(bytes("n Stupobot 8\n", 'UTF-8'))
+      self.readall()
+      self.s.send(bytes("b 8\n", 'UTF-8'))
+
+      self.s.send(bytes("u\nv %f\n" % self.cnt.speed, 'UTF-8'))
+
+   def step(self, cnt):
+      data = self.recvall(8)
+      typ, pid = struct.unpack("II", data)
+      if typ == 1:
+         self.cnt.mypid = pid
+         print ("My id: %d" % (self.cnt.mypid, ))
+      elif typ == 2:
+         print("Player %d left" % (pid, ))
+      elif typ == 3:
+         data = self.recvall(8)
+         fx, fy = struct.unpack("ff", data)
+         print("Player %d at %f, %f" % (pid, fx, fy))
+      elif typ == 5:
+         data = self.recvall(16)
+         a, v = struct.unpack("dd", data)
+         print("Player %d begun shot (v: %lf, a: %lf)" % (pid, v, a))
+         if pid == self.cnt.mypid:
+            self.cnt.e = self.cnt.e - v
+            self.cnt.angle = a + 370.5 / 5
+            if self.cnt.angle > 360:
+               self.cnt.angle = self.cnt.angle - 360
+            if self.cnt.e >= v:
+               self.s.send(bytes("%f\n" % self.cnt.angle, 'UTF-8'))
+            else:
+               self.s.send(bytes("u\n", 'UTF-8'))
+      elif typ == 6:
+         data = self.recvall(20)
+         a, v, n = struct.unpack("ddI", data)
+         print("Player %d finished shot (v: %lf, a: %lf) with %d segments" % (pid, v, a, n))
+         for i in range(n):
+            data = self.recvall(8)
+            #print("Curve: %r" % (struct.unpack("ff", data), ))
+      elif typ == 7:
+         data = self.recvall(4)
+         m = struct.unpack("I", data)[0]
+         if m != 3:
+            print("Unsupported gamemode %d, expect strangeness" % m)
+      elif typ == 8:
+         data = self.recvall(8)
+         self.cnt.e = struct.unpack("d", data)[0]
+         if self.cnt.e < 50:
+            time.sleep(2)
+            self.s.send(bytes("u\n", 'UTF-8'))
+         else:
+            self.s.send(bytes("%f\n" % self.cnt.angle, 'UTF-8'))
+      else:
+         print("Unexpected data: %r" % (data, ))
+
+         
+class Controller:
+   angle = 0
+   speed = 10
+   mypid = None
+   e = 0
+
+   def step(self):
+      self.net.step(self)
+
+   def __init__(self):
+      self.net = Network(self)
+
+      
 if __name__ == "__main__":
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("127.0.0.1", 3490))
+   cnt = Controller()
 
-    s.send(bytes("n Stupobot 8\n", 'UTF-8'))
-    readall(s)
-    s.send(bytes("b 8\n", 'UTF-8'))
+   while True:
+      cnt.step()
 
-    angle = 0
-    speed = 10
-    mypid = None
-    e = 0
-    s.send(bytes("u\nv %f\n" % speed, 'UTF-8'))
 
-    while True:
-       data = recvall(s, 8)
-       typ, pid = struct.unpack("II", data)
-       if typ == 1:
-          mypid = pid
-          print ("My id: %d" % (mypid, ))
-       elif typ == 2:
-          print ("Player %d left" % (pid, ))
-       elif typ == 3:
-          data = recvall(s, 8)
-          fx, fy = struct.unpack("ff", data)
-          print ("Player %d at %f, %f" % (pid, fx, fy))
-       elif typ == 5:
-          data = recvall(s, 16)
-          a, v = struct.unpack("dd", data)
-          print ("Player %d begun shot (v: %lf, a: %lf)" % (pid, v, a))
-          if pid == mypid:
-             e = e - v
-             angle = a + 370.5 / 5
-             if angle > 360:
-                angle = angle - 360
-             if e >= v:
-                s.send(bytes("%f\n" % angle, 'UTF-8'))
-             else:
-                s.send(bytes("u\n", 'UTF-8'))
-       elif typ == 6:
-          data = recvall(s, 20)
-          a, v, n = struct.unpack("ddI", data)
-          print ("Player %d finished shot (v: %lf, a: %lf) with %d segments" % (pid, v, a, n))
-          for i in range(n):
-             data = recvall(s, 8)
-             #print ("Curve: %r" % (struct.unpack("ff", data), ))
-       elif typ == 7:
-          data = recvall(s, 4)
-          m = struct.unpack("I", data)[0]
-          if m != 3:
-             print ("Unsupported gamemode %d, expect strangeness" % m)
-       elif typ == 8:
-          data = recvall(s, 8)
-          e = struct.unpack("d", data)[0]
-          if e < 50:
-             time.sleep(2)
-             s.send(bytes("u\n", 'UTF-8'))
-          else:
-             s.send(bytes("%f\n" % angle, 'UTF-8'))
-       else:
-          print ("Unexpected data: %r" % (data, ))
+
+
+
+
+
+
+
 
 
 
