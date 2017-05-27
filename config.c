51e995761fc5374b3b518ee45498aca5581d7fd2
Phil <Phil@foo.bar>
2017-05-27 14:32:13 +0200

    updated the help page

--- config.c
+++ config.c
@@ -21,7 +21,7 @@ void help(void)
    printf(" rate          energy increase rate (default 1.0/s)\n");
    printf(" limit         energy limit (default 200.0)\n");
    printf(" roundtime     time limit per round in s (default 600)\n");
-   printf(" gamemode      selects the gamemode (one out of: default, prefered, oldest, best\n");
+   printf(" extrapoints   selects the extrapoint mode (one out of: default, prefered, oldest, best\n");
    printf("\n");
    printf(" fullscreen    fullscreen enabled, default: 1\n");
    printf(" ratio         aspect ratio of battlefield (1.33, 4:3 and 4/3 are valid formats), default: 16:9\n");
